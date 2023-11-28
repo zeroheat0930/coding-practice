202eef7681862e6ebdfd807c3df4bb2ee0ae75ee
Jeong DongJun <80299089+zeroheat0930@users.noreply.github.com>
2023-11-28 10:57:23 +0900

    [level 2] Title: 교점에 별 만들기, Time: 10.15 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,92 +1,47 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-public class Solution {
-    // 0. 'Point' 클래스 정의
-    private static class Point {
-        public final long x, y;
-        
-        private Point(long x, long y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
-    // 1-A. 교점 좌표 구하기
-    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2){
-        //교점 구해서 반환하기
-        //문제에서 알려준 공식 그대로
-        double x = (double) (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
-        double y = (double) (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1);
-    
-        if(x % 1 != 0 || y % 1 != 0) return null;
-        return new Point((long) x, (long) y);
-    }
-    
-    // 2. 저장된 정수들에 대해 x,y 좌표의 최댓값, 최솟값 구하기
-    private Point getMinimumPoint(List<Point> points){
-        // 가장 작은 좌표 찾기
-        long x = Long.MAX_VALUE;
-        long y = Long.MAX_VALUE;
-        
-        for(Point p : points){
-            if(p.x < x) x = p.x;
-            if(p.y < y) y = p.y;
-        }
-        return new Point(x,y);
-    }
-    private Point getMaximumPoint(List<Point> points){
-        // 가장 큰 좌표 찾기
-        long x = Long.MIN_VALUE;
-        long y = Long.MIN_VALUE;
-        
-        for(Point p : points){
-            if(p.x > x) x = p.x;
-            if(p.y > y) y = p.y;
-        }
-        return new Point(x,y);
-    }
-    
-    
+class Solution {
+    static Set<List<Long>> intersection;
+    static long xMin, xMax, yMin, yMax;
     public String[] solution(int[][] line) {
-        // 1-B. 정수 좌표만 저장
-        List<Point> points = new ArrayList<>();
-        // 1. 모든 직선 쌍에 대해 반복
-        for(int i = 0; i < line.length; i++){
-            for(int j = 0; j < line.length; j++){
-                // line[i], line[j]를 이용하여 1-A, 1-B 수행
-                Point intersection = intersection(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
-                if(intersection != null){
-                    points.add(intersection);
-                }
+        intersection = new HashSet<>();
+        xMin = Long.MAX_VALUE; yMin = Long.MAX_VALUE;
+        xMax = Long.MIN_VALUE; yMax = Long.MIN_VALUE;
+
+        for(int i=0; i<line.length-1; i++){
+            for(int j=i+1; j<line.length; j++){
+                getIntersection(line[i], line[j]);
             }
         }
-        
-        // 3. 구한 최댓값, 최솟값을 이용하여 2차원 배열의 크기 결정
-        Point minimum = getMinimumPoint(points);
-        Point maximum = getMaximumPoint(points);
-        
-        int width = (int) (maximum.x - minimum.x + 1);
-        int height = (int) (maximum.y - minimum.y + 1);
-        
-        char [][] arr = new char[height][width];
-        for(char[] row : arr) {
-            Arrays.fill(row, '.');
+
+        String[] answer = new String[(int) (yMax - yMin) + 1];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = ".".repeat((int)(xMax - xMin) + 1);
         }
-        
-        for (Point p : points){
-            int x = (int)(p.x - minimum.x);
-            int y = (int)(maximum.y - p.y);
-            arr[y][x] = '*';
+
+        for(List<Long> star : intersection){
+            long x = star.get(0), y = star.get(1);
+            answer[(int)(yMax-y)] = answer[(int)(yMax-y)].substring(0, (int)(x-xMin)) + "*" + answer[(int)(yMax-y)].substring((int)(x-xMin)+1);
         }
-        
-        String [] result = new String[arr.length];
-        for (int i = 0; i < result.length; i++){
-            result[i] = new String(arr[i]);
+
+        return answer;
+    }
+
+    public void getIntersection(int[] line1, int[] line2){
+        double A, B, C, D, E, F;
+        A = line1[0]; B = line1[1]; E = line1[2];
+        C = line2[0]; D = line2[1]; F = line2[2];
+
+        double slope  = A*D - B*C;
+        if(slope == 0)  return;    
+
+        double x = (B*F - E*D) / slope, y = (E*C - A*F) / slope;
+        long ix = (long) x, iy = (long) y;
+        if(x == ix && y == iy){
+            intersection.add((List<Long>)Arrays.asList(new Long[] {ix, iy}));
+            xMin = Math.min(xMin, ix); xMax = Math.max(xMax, ix);
+            yMin = Math.min(yMin, iy); yMax = Math.max(yMax, iy);
         }
-        return result;
+        return;
     }
 }
-
-
