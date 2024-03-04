69d6d39bbf95fe42ba93486b912e6bfee771e32d
Jeong DongJun <80299089+zeroheat0930@users.noreply.github.com>
2024-03-04 15:40:10 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,20 +1,3 @@
 function solution(array) {
-    var answer = 0;
-    var len = array.length;
-    var middleIndex = Math.floor(len / 2); // 중간 인덱스 계산
-
-    // 배열을 크기대로 정렬
-    array.sort(function(a, b) {
-        return a - b;
-    });
-
-    // 배열의 중간 값을 반환
-    if (len % 2 === 1) { // 홀수 길이인 경우
-        answer = array[middleIndex];
-    } else { // 짝수 길이인 경우
-        // 중간 두 값의 평균 반환
-        answer = (array[middleIndex - 1] + array[middleIndex]) / 2;
-    }
-
-    return answer;
+  return array.sort((a, b) => a - b)[Math.floor(array.length / 2)];
 }
