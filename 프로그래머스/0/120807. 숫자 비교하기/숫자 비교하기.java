e251a4dc69a624bedeab3ce00cebfc9cca057a9d
Jeong DongJun <80299089+zeroheat0930@users.noreply.github.com>
2024-03-04 14:56:25 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,13 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        
-        if(num1 == num2){
-            answer = 1;
-        }else if(num1 != num2){
-            answer = -1;
-        }
-        
+        int answer = (num1 == num2) ? 1 : -1;
         return answer;
     }
 }
